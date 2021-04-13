0ec440b56cf170a41776c08f65373e7a76ec4477
Wayne Rocha <dev.waynerocha@gmail.com>
2021-04-13 15:09:35 -0300

    :ok_hand: refactor
    código simplificado

--- fire.js
+++ fire.js
@@ -1,19 +1,16 @@
 const firePixelArray = new Array;
 const fireWidth = 70;
 const fireHeight = 45;
+const tablePixels = createTableAndReturnCells();
 const fireColorsPalette = [
     {"r":7,"g":7,"b":7},{"r":31,"g":7,"b":7},{"r":47,"g":15,"b":7},{"r":71,"g":15,"b":7},{"r":87,"g":23,"b":7},{"r":103,"g":31,"b":7},{"r":119,"g":31,"b":7},{"r":143,"g":39,"b":7},{"r":159,"g":47,"b":7},{"r":175,"g":63,"b":7},{"r":191,"g":71,"b":7},{"r":199,"g":71,"b":7},{"r":223,"g":79,"b":7},{"r":223,"g":87,"b":7},{"r":223,"g":87,"b":7},{"r":215,"g":95,"b":7},{"r":215,"g":95,"b":7},{"r":215,"g":103,"b":15},{"r":207,"g":111,"b":15},{"r":207,"g":119,"b":15},{"r":207,"g":127,"b":15},{"r":207,"g":135,"b":23},{"r":199,"g":135,"b":23},{"r":199,"g":143,"b":23},{"r":199,"g":151,"b":31},{"r":191,"g":159,"b":31},{"r":191,"g":159,"b":31},{"r":191,"g":167,"b":39},{"r":191,"g":167,"b":39},{"r":191,"g":175,"b":47},{"r":183,"g":175,"b":47},{"r":183,"g":183,"b":47},{"r":183,"g":183,"b":55},{"r":207,"g":207,"b":111},{"r":223,"g":223,"b":159},{"r":239,"g":239,"b":199},{"r":255,"g":255,"b":255}
 ];
-let tablePixels = createTableAndReturnCells();
 let direction = false;
 let fireIntesity = 36;
-
-
 
 function start(){
     createFireEstructure();
     creatorFireResource();
-    calculateFirePropagation();
     setInterval(calculateFirePropagation, 60);
 }
 function createTableAndReturnCells(){
@@ -83,15 +80,15 @@ function addListnerToButtons(){
     buttons[1].addEventListener('click', lessFireIntensity);
     buttons[2].addEventListener('click', killFireIntensity);
     buttons[3].addEventListener('click', igniteFireIntesity);
-    buttons[4].addEventListener('click', function() { changeWindDirection(true) });
-    buttons[5].addEventListener('click', function() { changeWindDirection(false) });
+    buttons[4].addEventListener('click', () => changeWindDirection(true));
+    buttons[5].addEventListener('click', () => changeWindDirection(false));
 }
 function moreFireIntensity(){
-    fireIntesity += fireIntesity + 2 <= 36 ? 2 : 0;
+    fireIntesity += fireIntesity + 3 <= 36 ? 3 : 0;
     creatorFireResource();
 }
 function lessFireIntensity(){
-    fireIntesity -= fireIntesity - 2 > 0 ? 2 : 0;
+    fireIntesity -= fireIntesity - 3 > 0 ? 3 : 0;
     creatorFireResource();
 }
 function igniteFireIntesity(){
