3bf6f2af823b03dd50c8c7cbac7ab1e51ce90785
Wayne Rocha <dev.waynerocha@gmail.com>
2021-04-13 14:47:56 -0300

    :ok_hand: refactor
    Melhoria de desempenho
    ao invés de gerar uma nova tabela a cada loop, uma função criar a tabela e o loop apenas altera sua propriedade CSS

--- fire.js
+++ fire.js
@@ -1,9 +1,10 @@
 const firePixelArray = new Array;
-const fireWidth = 50;
-const fireHeight = 40;
+const fireWidth = 70;
+const fireHeight = 45;
 const fireColorsPalette = [
     {"r":7,"g":7,"b":7},{"r":31,"g":7,"b":7},{"r":47,"g":15,"b":7},{"r":71,"g":15,"b":7},{"r":87,"g":23,"b":7},{"r":103,"g":31,"b":7},{"r":119,"g":31,"b":7},{"r":143,"g":39,"b":7},{"r":159,"g":47,"b":7},{"r":175,"g":63,"b":7},{"r":191,"g":71,"b":7},{"r":199,"g":71,"b":7},{"r":223,"g":79,"b":7},{"r":223,"g":87,"b":7},{"r":223,"g":87,"b":7},{"r":215,"g":95,"b":7},{"r":215,"g":95,"b":7},{"r":215,"g":103,"b":15},{"r":207,"g":111,"b":15},{"r":207,"g":119,"b":15},{"r":207,"g":127,"b":15},{"r":207,"g":135,"b":23},{"r":199,"g":135,"b":23},{"r":199,"g":143,"b":23},{"r":199,"g":151,"b":31},{"r":191,"g":159,"b":31},{"r":191,"g":159,"b":31},{"r":191,"g":167,"b":39},{"r":191,"g":167,"b":39},{"r":191,"g":175,"b":47},{"r":183,"g":175,"b":47},{"r":183,"g":183,"b":47},{"r":183,"g":183,"b":55},{"r":207,"g":207,"b":111},{"r":223,"g":223,"b":159},{"r":239,"g":239,"b":199},{"r":255,"g":255,"b":255}
 ];
+let tablePixels = createTableAndReturnCells();
 let direction = false;
 let fireIntesity = 36;
 
@@ -13,8 +14,21 @@ function start(){
     createFireEstructure();
     creatorFireResource();
     calculateFirePropagation();
-
-    setInterval(calculateFirePropagation, 30);
+    setInterval(calculateFirePropagation, 60);
+}
+function createTableAndReturnCells(){
+    let table = '<table cellspacing="0" cellpadding="0" id="fireTable">';
+    for (let row = 0; row < fireHeight; row++){
+        table += '<tr>';
+        for (let column = 0; column < fireWidth; column++){
+            table += `<td>`;
+            table += '</td>'
+        }
+        table += '</tr>';
+    }
+    table += '</table>';
+    document.getElementById('fogo').innerHTML = table;
+    return document.querySelectorAll('#fireTable td');
 }
 function createFireEstructure(){
     const numberOfPixels = fireWidth * fireHeight;
@@ -53,21 +67,15 @@ function updateFireIntesityPerPixel(currentPixelIndex){
     }
 }
 function renderFire(){
-    let table = '<table cellspacing="0" cellpadding="0">';
     for (let row = 0; row < fireHeight; row++){
-        table += '<tr>';
         for (let column = 0; column < fireWidth; column++){
             const pixelIndex = column + row * fireWidth;
             const fireIntensity = firePixelArray[pixelIndex];
-            let palette = fireColorsPalette[fireIntensity];
-            let rgbColor = `${palette.r}, ${palette.g}, ${palette.b}`;
-            table += `<td style="background-color: rgb(${rgbColor});"`;
-            table += '</td>'
+            const palette = fireColorsPalette[fireIntensity];
+            const rgbColor = `${palette.r}, ${palette.g}, ${palette.b}`;
+            tablePixels[pixelIndex].style.backgroundColor = `rgb(${rgbColor})`;
         }
-        table += '</tr>';
     }
-    table += '</table>';
-    document.getElementById('fogo').innerHTML = table;
 }
 function addListnerToButtons(){
     const buttons = document.querySelectorAll('button');
